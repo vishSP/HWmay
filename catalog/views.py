dca52f23a41355272dbba5266273d21759f4a1ec
vishnSP <hihi64741@gmail.com>
2023-05-02 23:19:52 +0400

    сделал домашку

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def contacts(request):
+    return render(request, 'catlog/contacts')
+
