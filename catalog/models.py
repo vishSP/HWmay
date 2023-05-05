a36768491ccbb8496b81d2e1d1802a1a5c54af14
vishnSP <hihi64741@gmail.com>
2023-05-05 19:46:58 +0400

    дописал класс Meta

--- catalog/models.py
+++ catalog/models.py
@@ -3,20 +3,25 @@ from django.db import models
 # Create your models here.
 NULLABLE = {'blank': True, 'null': True}
 
+
 class Category(models.Model):
-    category_name = models.CharField(max_length=50,verbose_name='название категории')
+    category_name = models.CharField(max_length=50, verbose_name='название категории')
     category_description = models.TextField(verbose_name='описание')
 
     def __str__(self):
         return self.category_name
 
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('category_name',)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='наименование')
     description = models.CharField(max_length=50, verbose_name='описание')
     image = models.ImageField(upload_to='product', verbose_name='изображение', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
     date_create = models.DateField(verbose_name='дата создания')
     date_last = models.DateField(verbose_name='дата последнего изменения ')
@@ -24,4 +29,7 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.name}{self.price}{self.category}'
 
-
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
